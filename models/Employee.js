b5bffd17d19aff9d42a2eae8553f4d6f9640836a
Hervinho <herventubi@gmail.com>
2017-12-20 14:07:21 +0200

    Unique...

--- models/Employee.js
+++ models/Employee.js
@@ -1,4 +1,5 @@
 var connection = require('../config/connection');
+var SHA256 = require("crypto-js/sha256");
 
 function Employee() {
     //get all employees
@@ -73,19 +74,30 @@ function Employee() {
         });
     };
 
+    //Employee login
+    this.login = function(employeeObj, res){
+        var output = {},
+        feedback, query = "SELECT * FROM employee WHERE";
+    };
+
     //create employee.
     this.create = function (employeeObj, res) {
         var output = {},
-            feedback, query = "INSERT INTO employee VALUES(?,?,?,?,?,?)";
+            feedback, query = "INSERT INTO employee VALUES(?,?,?,?,?,?,?,?,?)";
         var employee_id_number = employeeObj.employee_id_number,
             employee_name = employeeObj.employee_name,
             employee_gender_id = employeeObj.employee_gender_id,
             employee_role_id = employeeObj.employee_role_id,
-            employee_code = employeeObj.employee_code;
+            employee_code = employeeObj.employee_code,
+            employee_phone = employeeObj.employee_phone,
+            employee_email = employeeObj.employee_email,
+            employee_password = String(employeeObj.employee_password);
 
         if ((undefined !== employee_id_number && employee_id_number != '') && (undefined !== employee_name && employee_name != '') &&
             (undefined !== employee_gender_id && employee_gender_id != '') && (undefined !== employee_role_id && employee_role_id != '') &&
-            (undefined !== employee_code && employee_code != '')) {
+            (undefined !== employee_code && employee_code != '') && (undefined !== employee_phone && employee_phone != '') && 
+            (undefined !== employee_email && employee_email != '') && (undefined !== employee_password && employee_password != '')
+        ) {
             connection.acquire(function (err, con) {
                 if (err) {
                     res.json({
@@ -95,10 +107,23 @@ function Employee() {
                     return;
                 }
 
-                con.query(query, [null, employee_id_number, employee_name, employee_gender_id, employee_role_id, employee_code], function (err, result) {
+                employee_password = SHA256(employee_password).toString();
+
+                con.query(query, [null, employee_id_number, employee_name, employee_gender_id, employee_role_id, employee_code, employee_phone, employee_email, employee_password], function (err, result) {
                     con.release();
                     if (err) {
-                        res.json(err);
+                        //console.log(err);
+                        if(err.code == 'ER_DUP_ENTRY'){
+                            output = {
+                                status: 0,
+                                message: 'Same IDnumber/Code/email/phone number already exists',
+                                error: err
+                            };
+                        }
+                        else{
+                            output = err;
+                        }
+                        res.json(output);
                     } else {
                         feedback = 'Employee successfully added';
                         output = {
@@ -123,18 +148,20 @@ function Employee() {
     //update employee.
     this.update = function (employeeObj, res) {
         var output = {},
-            feedback, query = "UPDATE employee SET employee_name=?, employee_gender_id=?, employee_role_id=?, employee_code=? " +
-            "WHERE employee_id=?";
-        var employee_id_number = employeeObj.employee_id_number,
-            employee_name = employeeObj.employee_name,
+            feedback, query = "UPDATE employee SET employee_name=?, employee_gender_id=?, employee_role_id=?, employee_code=?, " +
+            "employee_phone=?, employee_email=? WHERE employee_id=?";
+        var employee_name = employeeObj.employee_name,
             employee_gender_id = employeeObj.employee_gender_id,
             employee_role_id = employeeObj.employee_role_id,
             employee_code = employeeObj.employee_code,
-            employee_id = employeeObj.employee_id;
+            employee_id = employeeObj.employee_id,
+            employee_phone = employeeObj.employee_phone,
+            employee_email = employeeObj.employee_email;
 
-        if ((undefined !== employee_id_number && employee_id_number != '') && (undefined !== employee_name && employee_name != '') &&
-            (undefined !== employee_gender_id && employee_gender_id != '') && (undefined !== employee_role_id && employee_role_id != '') &&
-            (undefined !== employee_code && employee_code != '') && (undefined !== employee_id && employee_id != '')) {
+        if ((undefined !== employee_name && employee_name != '') && (undefined !== employee_gender_id && employee_gender_id != '') && (undefined !== employee_role_id && employee_role_id != '') &&
+            (undefined !== employee_code && employee_code != '') && (undefined !== employee_id && employee_id != '') &&
+            (undefined !== employee_phone && employee_phone != '') && (undefined !== employee_email && employee_email != '')
+        ) {
             connection.acquire(function (err, con) {
                 if (err) {
                     res.json({
@@ -144,16 +171,29 @@ function Employee() {
                     return;
                 }
 
-                con.query(query, [employee_id_number, employee_name, employee_gender_id, employee_role_id, employee_code, employee_id], function (err, result) {
+                con.query(query, [employee_name, employee_gender_id, employee_role_id, employee_code, employee_phone, employee_email, employee_id], function (err, result) {
                     con.release();
                     if (err) {
-                        res.json(err);
+                        //console.log(err);
+                        if(err.code == 'ER_DUP_ENTRY'){
+                            output = {
+                                status: 0,
+                                message: 'Same IDnumber/Code/email/phone number already exists',
+                                error: err
+                            };
+                        }
+                        else{
+                            output = err;
+                        }
+                        res.json(output);
+                        
                     } else {
+                        //console.log(result);
                         feedback = 'Employee successfully updated';
                         output = {
                             status: 1,
                             message: feedback,
-                            updatedEmployeeIdNumber: employee_id_number
+                            updatedEmployeeId: employee_id
                         };
                         res.json(output);
                     }
