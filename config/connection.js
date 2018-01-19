04bd35962826aa69c398febb7d8943691efdf4ba
Hervinho <herventubi@gmail.com>
2018-01-19 10:51:37 +0200

    rebase

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,8 @@ function Connection() {
             connectionLimit: 1500,
             host: '127.0.0.1',
             user: 'root',
-            password: '',
+            password: 'aXWSIWFePYs9',
+            //password: '',
             dateStrings: 'date',
             database: 'pheme'
         });
